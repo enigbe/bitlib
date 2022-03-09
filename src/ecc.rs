70a00502e100e9eec571c57199929c5eb6a24c20
enigbe <ochekliyeenigbe@gmail.com>
2022-03-09 12:18:51 +0100

    feat(ecc): FieldElement configuration with 32-bit

--- src/ecc.rs
+++ src/ecc.rs
@@ -1,6 +1,6 @@
 /// Module containing helper primitives for Elliptic Curve Cryptography (ECC)
 use std::fmt::{Display, format, Formatter, Result as FmtResult};
-use std::ops::Add;
+use std::ops::{Add, Sub, Mul, Div};
 
 /// A element belonging to a finite set of prime order
 #[derive(Debug, PartialEq)]
@@ -16,14 +16,14 @@ pub enum ECCError {
     ValueError(String),
 }
 
-/// Implements display for a FieldElement
+/// Implements display trait for FieldElement
 impl Display for FieldElement {
     fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
         write!(f, "({}, {})", self.num, self.prime)
     }
 }
 
-/// Implements addition for a FieldElement
+/// Implements addition trait for a FieldElement
 impl Add for FieldElement {
     type Output = Result<FieldElement, ECCError>;
 
@@ -32,7 +32,7 @@ impl Add for FieldElement {
             let error_message = "Cannot add two elements in different fields".to_owned();
             Err(ECCError::TypeError(error_message))
         } else {
-            let num = (self.num + rhs.num) % self.prime;
+            let num = modulus(self.num + rhs.num, self.prime);
             Ok(FieldElement {
                 num,
                 prime: self.prime,
@@ -41,8 +41,69 @@ impl Add for FieldElement {
     }
 }
 
+/// Implements subtraction trait for FieldElement
+impl Sub for FieldElement {
+    type Output = Result<FieldElement, ECCError>;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            let error_message = "Cannot subtract two elements in different fields".to_owned();
+            Err(ECCError::TypeError(error_message))
+        } else {
+            let num = modulus(self.num - rhs.num, self.prime);
+            Ok(FieldElement {
+                num,
+                prime: self.prime,
+            })
+        }
+    }
+}
+
+/// Implement multiplication trait for FieldElement
+impl Mul for FieldElement {
+    type Output = Result<FieldElement, ECCError>;
+    
+    fn mul(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            let error_message = "Cannot multiply two number in different fields".to_owned();
+            Err(ECCError::TypeError(error_message))
+        } else {
+            let num = modulus(self.num * rhs.num, self.prime);
+            Ok(FieldElement {
+                num,
+                prime: self.prime,
+            })
+        }
+    }
+}
+
+/// Implement division trait for FieldElement
+impl Div for FieldElement {
+    type Output = Result<FieldElement, ECCError>;
+
+    fn div(self, rhs: Self) -> Self::Output {
+        if self.prime != rhs.prime {
+            let error_message = "Cannot divide two number in different fields".to_owned();
+            Err(ECCError::TypeError(error_message))
+        } else {
+            let fermat_pos: u32 = (self.prime - 2).try_into().unwrap();
+            let a: i32 = rhs.num.pow(fermat_pos);
+
+            let divisor = modulus(a, self.prime);
+            let dividend = self.num * divisor;
+            let num = modulus(dividend, self.prime);
+
+            Ok(FieldElement{
+                num,
+                prime: self.prime,
+            })
+        }
+    }
+}
+
+/// Implement methods for FieldElement
 impl FieldElement {
-    /// Create a new FieldElement
+    /// Create a new FieldElement with integer num and prime
     pub fn new(num: i32, prime: i32) -> Result<Self, ECCError> {
         if num >= prime || num < 0 {
             let error_message = format(format_args!("Num {} not in the field range 0 to {}", num, prime - 1));
@@ -54,5 +115,21 @@ impl FieldElement {
             })
         }
     }
+
+    /// Raise the power of the FieldElement to the given exponent
+    pub fn pow(&self, exponent: i32) -> Result<Self, ECCError> {
+        let n = modulus(exponent, self.prime - 1);
+        let n_pos = n.try_into().unwrap();
+        let num = modulus(self.num.pow(n_pos), self.prime);
+
+        FieldElement::new(num, self.prime)
+    }
+}
+
+/// Modulo workaround: Helper function to compute the modulo that wraps around
+/// unlike the remainder that '%' computes
+fn modulus(a: i32, b: i32) -> i32 {
+    let modu = ((a % b) + b) % b;
+    modu
 }
 
--- tests/ecc_test.rs
+++ tests/ecc_test.rs
@@ -1,70 +1,138 @@
 use ecc::{FieldElement};
 
-/// test create valid FieldElement
+/// test the creation of valid FieldElement
 #[test]
 fn test_valid_create_field_element() {
-    let valid_field_element = FieldElement::new(5, 19);
-    let invalid_field_element = FieldElement::new(20, 19);
+    let valid_fe_1 = FieldElement::new(5, 19);
+    let invalid_fe_1 = FieldElement::new(20, 19);
+    let invalid_fe_2 = FieldElement::new(-6, 21);
 
-    match valid_field_element {
-        Ok(result) => {
-            assert_eq!(result.num, 5);
-            assert_eq!(result.prime, 19);
-            assert!(result.num < result.prime - 1);
+    match valid_fe_1 {
+        Ok(res) => {
+            assert_eq!(res.num, 5);
+            assert_eq!(res.prime, 19);
         },
-        Err(_e) => (),
-    }
+        Err(_e) => {
+            println!("Invalid FieldElement");
 
-    match invalid_field_element {
-        Ok(_) => (),
-        Err(e) => {
-            println!("Invalid FieldElement {:?}", e);
         },
     }
+
+    match invalid_fe_1 {
+        Ok(_res) => {},
+        Err(_e) => {
+            println!("Invalid FieldElement");
+        },
+    }
+
+    match invalid_fe_2 {
+        Ok(_res) => {},
+        Err(_e) => {
+            println!("Invalid FieldElement");
+        },
+    }
+
 }
 
 /// test the equality of two field elements
 #[test]
 fn test_field_element_are_equal() {
-    let field_element_one = FieldElement::new(5, 19);
-    let field_element_two = FieldElement::new(5, 19);
+    let field_element_one = FieldElement::new(5, 19).unwrap();
+    let field_element_two = FieldElement::new(5, 19).unwrap();
 
-    match field_element_one {
-        Ok(result) => {
-            match field_element_two {
-                Ok(result_two) => {
-                    assert_eq!(result, result_two);
-                },
-                Err(_e) => ()
-            }
-        },
-        Err(_e) => ()
-    }
+    assert_eq!(field_element_one, field_element_two);
 }
 
 /// test the addition of two field elements
 #[test]
 fn test_add_two_field_element() {
     // set up
-    let field_element_one = FieldElement::new(5, 19).expect("Can't create FieldElement");
-    let field_element_two = FieldElement::new(5, 19).expect("Can't create FieldElement");
+    let field_element_one = FieldElement::new(5, 19).unwrap();
+    let field_element_two = FieldElement::new(5, 19).unwrap();
 
-    let expected = FieldElement::new(10, 19);
+    let expected = FieldElement::new(10, 19).unwrap();
 
     // act
     let sum = field_element_one + field_element_two;
 
     match sum {
         Ok(actual) => {
-            match expected {
-                Ok(expect) => {
-                    // assert
-                    assert_eq!(actual, expect);
-                },
-                Err(_e) => (),
-            }
+            assert_eq!(actual, expected);
         },
         Err(_e) => (),
     }
 }
 
+/// test the subtraction of two field elements
+#[test]
+fn test_subtract_two_field_element() {
+    // set up
+    let fe_1 = FieldElement::new(5, 19).unwrap();
+    let fe_2 = FieldElement::new(7, 19).unwrap();
+    let expected = FieldElement::new(17, 19).unwrap();
+
+    // act
+    let diff = fe_1 - fe_2;
+
+    match diff {
+        Ok(res) => {
+            assert_eq!(res, expected);
+        },
+        Err(_e) => (),
+    }
+
+}
+
+/// test the multiplication of two field elements
+#[test]
+fn test_multiply_two_field_elements() {
+    // arrange
+    let fe_1 = FieldElement::new(12, 23).unwrap();
+    let fe_2 = FieldElement::new(17, 23).unwrap();
+    let expected = FieldElement::new(20, 23).unwrap();
+
+    // act
+    let mul = fe_1 * fe_2;
+
+    match mul {
+        Ok(res) => {
+            // assert
+            assert_eq!(res, expected);
+        },
+        Err(_e) => (),
+    }
+}
+
+/// test the exponentiation of a FieldElement to a given exponent
+#[test]
+fn test_raise_field_element_to_power() {
+    let exp = -5;
+    let fe_1 = FieldElement::new(5, 7);
+    let expected = FieldElement::new(5, 7).unwrap();
+
+    match fe_1 {
+        Ok(res) => {
+            let power = res.pow(exp).unwrap();
+            assert_eq!(power, expected);
+        },
+        Err(_e) => (),
+    }
+
+}
+
+/// test the division of a FieldElement with another
+#[test]
+fn test_division_of_two_field_elements() {
+    let fe_1 = FieldElement::new(5, 19).unwrap();
+    let fe_2 = FieldElement::new(17, 19).unwrap();
+    let expected = FieldElement::new(7, 19).unwrap();
+
+    let div = fe_1 / fe_2;
+
+    match div {
+        Ok(res) => {
+            assert_eq!(res, expected);
+        },
+        Err(_e) => (),
+    }
+}
